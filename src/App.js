939d0a45fe3429685c5bb7ebf4c469a7b97ac795
aidarous <aidarous12@gmail.com>
2020-12-17 14:07:09 -0500

    rendered trivia to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import './components/Questions';
+import Questions from './components/Questions';
 
 function App() {
   return (
     <div className="App">
      <h1>Welcome to trivia world</h1>
-     
+     < Questions />
     </div>
   );
 }
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 
-const Questions = ["What city does LeBron james play in?", "In what division are the Dallas Mavericks?"," What team played in Oakland and now plays in San Francisco?"]
+const trivia = ["What city does LeBron james play in?", "In what division are the Dallas Mavericks?"," What team played in Oakland and now plays in San Francisco?"]
+console.log(trivia);
 class Questions extends Component {
     render() {
         return (
             <div>
-                
+                <p>{trivia[0]}</p>
+                <p>{trivia[1]}</p>
+                <p>{trivia[2]}</p>
+
             </div>
         );
     }
