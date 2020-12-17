928f3cbb47e9ba9295ddcd504e3821860c4e7067
aidarous <aidarous12@gmail.com>
2020-12-17 00:38:59 -0500

    added trivia to Questions.js

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 
 
-const Questions = 
-{}
+const Questions = ["What city does LeBron james play in?", "In what division are the Dallas Mavericks?"," What team played in Oakland and now plays in San Francisco?"]
 class Questions extends Component {
     render() {
         return (
