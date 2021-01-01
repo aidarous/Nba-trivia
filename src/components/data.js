c6051af5e98315527a23f7581f43f970486da9d6
aidarous <aidarous12@gmail.com>
2020-12-31 19:29:16 -0500

    added answer choices to data.js

--- src/components/data.js
+++ src/components/data.js
@@ -1,11 +1,19 @@
 const trivia = [
     {
         id:1,
-        question:"What city does LeBron james currently play in?",
+        question:"What city does LeBron james currently play in currently?",
+        choice1: "Miami",
+        choice2: "Los Angeles",
+        choice3: "Cleveland",
+        choice4: "New York",
     }, 
     {
         id:2,
         question:"In what division are the Dallas Mavericks?",
+        choice1:"Atlantic",
+        choice2:"Pacific",
+        choice3:"Southwest",
+        choice4:"Northwest",
     }, 
     {
         id:3,
